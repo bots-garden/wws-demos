2299df74e4f61d6874155fe6e5625c400bb78e22
k33g <ph.charriere@gmail.com>
2023-07-29 12:21:30 +0200

    💾 Saved.

--- experiments/build.sh
+++ experiments/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#wget https://github.com/vmware-labs/wasm-workers-server/releases/download/v1.4.0/wws-linux-musl-aarch64.tar.gz
-#tar -xzf wws-linux-musl-aarch64.tar.gz
-#rm wws-linux-musl-aarch64.tar.gz
+wget https://github.com/vmware-labs/wasm-workers-server/releases/download/v1.4.0/wws-linux-musl-aarch64.tar.gz
+tar -xzf wws-linux-musl-aarch64.tar.gz
+rm wws-linux-musl-aarch64.tar.gz
 
-wget https://github.com/vmware-labs/wasm-workers-server/releases/download/v1.4.0/wws-linux-musl-x86_64.tar.gz
-tar -xzf wws-linux-musl-x86_64.tar.gz
-rm wws-linux-musl-x86_64.tar.gz
+#wget https://github.com/vmware-labs/wasm-workers-server/releases/download/v1.4.0/wws-linux-musl-x86_64.tar.gz
+#tar -xzf wws-linux-musl-x86_64.tar.gz
+#rm wws-linux-musl-x86_64.tar.gz
 
 cp ../01-first-go-worker/worker.wasm worker.wasm 
 
@@ -13,3 +13,7 @@ IMAGE_NAME="demo-wws"
 docker build -t ${IMAGE_NAME} . 
 
 docker images | grep ${IMAGE_NAME}
+
+rm README.md
+rm LICENSE
+
